2da11129d2832a50d7b814d66c460484c3a5e3f9
Charles Ewart <40551334+ccewart@users.noreply.github.com>
2018-07-13 11:12:23 +1000

    Delete test01.py

--- test01.py
+++
@@ -1,5 +0,0 @@
-from environment import *
-
-Map = Environment()
-Grid_square = namedtuple('Grid_square', 'coords discovered land water wall tree treasure axe key stone heuristic')
-coord = Grid_square(coords = (1,1), land = 'True', discovered = 'True')
